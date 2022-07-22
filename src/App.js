0cbcf4acb24996e4cdfcc33b1accdee9a2ac730d
MiryamBathilde <mbathilde@gmail.com>
2022-07-22 20:42:37 +0200

    instroduction to props, and functional and class components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from "./components/pure/greeting";
+import GreetingFuncion from "./components/pure/greetingFuncion";
 
-function App ()
-{
+function App () {
   return (
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <p>
-          !Hola Mundo!
+          {/* components */ }
+          {/* name es la props de grettings */ }
+          {/* <Greeting name="Miryam"></Greeting> */ }
+          <GreetingFuncion name="Miryam"></GreetingFuncion>
         </p>
       </header>
     </div>
